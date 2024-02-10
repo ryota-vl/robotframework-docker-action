93e699a68f6bd52994bb581ca30fb55eb4b7893f
wclee <msc0953@gmail.com>
2024-02-10 23:57:34 +0800

    fix: resources folder exists

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
-# Create reports folder
-REPORTS_DIR=$(pwd)/$ROBOT_REPORTS_DIR
-TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
-RESOURCES_DIR=$(pwd)/$ROBOT_RESOURCES_DIR
 
-sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
+sudo mkdir reports && sudo chmod 777 reports
 
 docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   --user=1001:1001 \
@@ -11,7 +7,7 @@ docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e ROBOT_THREADS=$ROBOT_THREADS \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
-  -v $REPORTS_DIR:/opt/robotframework/reports:Z \
-  -v $TESTS_DIR:/opt/robotframework/tests:Z \
-  -v $RESOURCES_DIR:/opt/robotframework/resources:Z \
+  -v reports:/opt/robotframework/reports:Z \
+  -v tests:/opt/robotframework/tests:Z \
+  -v resources:/opt/robotframework/resources:Z \
   $ROBOT_RUNNER_IMAGE
